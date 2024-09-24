0be69486b1b30420830bb61aeab0be1a625f4b8a
LorenzoPrince <lorenzoprince222@gmail.com>
2024-09-24 13:58:07 -0300

    ready cam
    listo camara

--- Assets/Scrips/Camere.cs
+++ Assets/Scrips/Camere.cs
@@ -27,13 +27,21 @@ public class Camere : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mouseInput = new Vector2(Input.GetAxisRaw("House X"), Input.GetAxisRaw("House Y"));
+        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
 
         mouseInput = Vector2.Scale(mouseInput, new Vector2(sensibility * smooth, sensibility * smooth));
 
         smoothV.x = Mathf.Lerp(smoothV.x, mouseInput.x, 1f / smooth);
         smoothV.y = Mathf.Lerp(smoothV.y, mouseInput.y, 1f / smooth);
+        
+        rotationX -= smoothV.y;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
 
+        rotationY += smoothV.x;
+
+        transform.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
+        
+        player.transform.localRotation = Quaternion.Euler(0f, rotationY, 0f);
 
 
     }
