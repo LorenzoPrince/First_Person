15266192015124c932100094d97341e3c182522d
LorenzoPrince <lorenzoprince222@gmail.com>
2024-12-15 20:56:19 -0300

    Weapon and map
    Arregle el arma  para que cuando colisione contra algo se rompan. Tambien le di apareciencia al prefab de la bala con color y agrengado partículas para que cuando inpacte la bala contra un objeto salga unas partícula de restos. Tambien cambie de lugar la casa y le puse referencias de terror en el mapa como frases fear y run

--- Assets/Scrips/Old game/Bullet.cs
+++ Assets/Scrips/Old game/Bullet.cs
@@ -6,7 +6,22 @@ public class Bullet : MonoBehaviour
 {
     // Start is called before the first frame update
 
-
+    [SerializeField] private GameObject impactVFXPrefab;
     public int damage = 25;
 
+    private void OnCollisionEnter(Collision contraLoQueChoque)
+    {
+        if (impactVFXPrefab != null)
+        {
+            // Obtener el punto de contacto de la colisión
+            ContactPoint contacto = contraLoQueChoque.contacts[0];
+            // Instanciar el efecto de partículas en el lugar donde ocurrió la colisión
+            GameObject impactVFXClone = Instantiate(impactVFXPrefab, contacto.point, Quaternion.LookRotation(contacto.normal));
+            Destroy(impactVFXClone, 1f);  // Destruir las partículas después de 1 segundo
+        }
+        Debug.Log("la Bala choco con: " + contraLoQueChoque.gameObject.name);
+        Destroy(gameObject);
+    }
+
 }
+
--- Assets/Scrips/Old game/Spawn.cs
+++ Assets/Scrips/Old game/Spawn.cs
@@ -9,6 +9,7 @@ public class Spawn : MonoBehaviour
     [SerializeField] private GameObject objectPrefab;
     [SerializeField] private GameObject shootVFXPrefab; // Prefab de las partículas del disparo
     public Vector3 spawnPosition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +22,8 @@ public class Spawn : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             GameObject bulletClone = Instantiate(objectPrefab, transform.position, transform.rotation); // el quaternion. identity es para que sea en la misma posicion que el vector . y el vector que esta el spawn
-            
+ 
+
             Rigidbody bulletRigidBody = bulletClone.GetComponent<Rigidbody>(); //rigid body de la nueva bala
 
             bulletRigidBody.velocity = transform.forward * bulletForce; // impulso de la bala multiplicado por el bulletforce
@@ -31,10 +33,10 @@ public class Spawn : MonoBehaviour
             // Instanciar el efecto de partículas en la misma posición del disparo
             Destroy(shootVFXClone, 0.5f);
             Destroy(bulletClone, 2f);
-
+            // en el prefab de la bala hago que si se choca contra algo se elimine la bala pero esto en el prefab
         }
-        private void OnCollisionEnter(Collision contraLoQueChoque)
-
-
     }
+
+
+    
 }
